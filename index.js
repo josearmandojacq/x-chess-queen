0f5b83413f7289064853f4df25f768481bc47725
Jose Armando Castillo Quiala <josearmandocastilloquiala@Joses-MBP.coworking.lan>
2017-08-21 09:31:49 +0200

    update ;)

--- index.js
+++ index.js
@@ -35,119 +35,16 @@ function renderChessBoard(table){
   }
 
 
-  // //// putting the color to the board////// need some changes //////////
-  // for (var i = 0; i < 8; i++) {
-  //   for (var j = 0; j < 8; j++) {
-  //     if((i - j) % 2 !== 0){
-  //       document.getElementsByTagName('tr')[i].cells[j].classList.add("black");
-  //     }
-  //   }
-  // }
+  
 
   return matrix;
 }
 
 
-///////////// Hightlighing a Row ////////////////////
 
-function highlightRow(item){
-  var parent = item.path[0].parentNode;
-  parent.classList.add("highlight");
-
-}
-
-/////////// Hightlighing a Column //////////////////
-
-function highlightColumn(matrix, rowNum){
-  matrix.forEach(function(rowArray){
-    rowArray[rowNum].classList.add("highlight");
-
-  });
-}
-
-///////// Remove Highlighting ///////////////
-
-function removeHighlight(matrix){
-  var rows = document.querySelectorAll("tr");
-  var columns = document.querySelectorAll("td");
-
-  for (var i = 0; i < 8; i++) {
-    rows[i].classList.remove("highlight");
-  }
-  for (var i = 0; i < 64; i++) {
-    columns[i].classList.remove("highlight");
-  }
-}
-
- ///////////////// Highlighting the diagonals ///////////
-
- function highlightDiagonals(matrix, rowNum,columNum){
-   var temp = [];
-   var i = rowNum;
-   var j = columNum;
-
-      while(i >= 0 && j >= 0){
-        if(matrix[i][j]){
-          temp.push(matrix[i][j]);
-
-        }
-        i--;
-        j--;
-      }
-
-      var i = rowNum;
-      var j = columNum;
-
-      while(i >= 0 && j <= 8){
-        if(matrix[i][j]){
-          temp.push(matrix[i][j]);
-
-        }
-        i--;
-        j++;
-      }
-
-      var i = rowNum;
-      var j = columNum;
-
-      while(i <= 7 && j >= 0){
-        if(matrix[i][j]){
-          temp.push(matrix[i][j]);
-
-        }
-        i++;
-        j--;
-      }
-
-      var i = rowNum;
-      var j = columNum;
-
-      while(i <= 7 && j <= 7){
-
-        if(matrix[i][j]){
-          temp.push(matrix[i][j]);
-        }
-        i++;
-        j++;
-      }
+ ///////////////// Highlighting ///////////
 
 
-
-
-
-       /*if(matrix[rowNum][columNum]){
-         temp.push(matrix[rowNum][columNum]);
-         rowNum++;
-         columNum++;
-       }*/
-       temp.forEach(function(item){
-         item.classList.add("highlight");
-       });
-
-
-
-   console.log(temp);
- }
 
 function highlight2(matrix, rowNum, columNum) {
   matrix.forEach(function(row, rowIdx) {
